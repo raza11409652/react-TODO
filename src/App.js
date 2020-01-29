2889a846ae4396f6e6a846cca86cf9401696b92f
Hackdrid <raza.11409652@lpu.in>
2020-01-29 16:09:28 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
+import Note from './Component/Note';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      noteText:'',
+      notes:[],
+    };
+  }
+  updateSearch = e=>{
+   this.setState({
+    noteText:e.target.value
+    }) ; 
+    // console.log(search);
+    console.log(this.state.noteText);
+  }
+
+  handleKeyPress = (event)=>{
+      if(event.key === "Enter"){
+        console.log("Press enter");
+      this.addNode()
+      }
+  }
+  addNode = ()=>{
+    if(this.state.noteText=== ''){
+      return 
+    }
+    let notesArray = this.state.notes;
+    notesArray.push(this.state.noteText);
+    this.setState({noteText:''})
+   
+  }
+  deleteNote = (index)=>{
+    let noteAttr  =this.state.notes ; 
+    noteAttr.splice(index , 1);
+    this.setState({
+      notes:noteAttr
+    })
+  }
+  render(){
+
+    let notes = this.state.notes.map((val , key) => {
+      return <Note key = {key} text={val}  deleteMethod={
+        ()=>this.deleteNote(key)
+      }/>
+    }) ;
+    return(
+
+      <div className="container">
+          <div className="header">
+              React Todo APP
+          </div>
+          {notes}
+          <div className="btn" onClick={this.addNode.bind(this)}>+</div>
+          <input type="text" 
+          placeholder="Text here"
+          className="textInput"
+           value={this.state.noteText}
+           onChange={this.updateSearch} 
+           onKeyPress={this.handleKeyPress.bind(this)}/>
+      </div>
+    );
+  }
 }
 
 export default App;
